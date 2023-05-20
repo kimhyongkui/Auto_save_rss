0d2514934a85f56efda6ad81e25be46dbb1b9308
Kim Hyeong Kyu <hyongkui@nate.com>
2023-05-20 23:24:35 +0900

    새 프로젝트 시작

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+import uvicorn
+
+app = FastAPI(title="item-analysis")
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="localhost",
+        reload=True,
+        port=8050
+    )
+
+
+@app.post("/")
+def main():
+    result = "hello"
+    return result
