35e53740e26d670d1d51a7c670dfce39947f9aed
Kim Hyeong Kyu <hyongkui@nate.com>
2023-05-31 18:58:37 +0900

    rss_list의 이름을 dic으로 잘못적어놓은거 수정 & 다음에 할일 주석

--- api/parse_rss.py
+++ api/parse_rss.py
@@ -15,20 +15,21 @@ def parsing_rss():
         newsis,
         itworld
     ]
-    rss_dic = []
+    rss_list = []
 
     for url in url_list:
         parse_rss = feedparser.parse(url)
-
+        # 지금 코드를 그대로 사용하면 RSS 피드 메인페이지의 목록을 그대로 긁어옴.
+        # 내가 원하는 날짜의 기사만 필터링 할 필요가 있다.
         for entry in parse_rss.entries:
             title = entry.title if hasattr(entry, "title") else None
             link = entry.link if hasattr(entry, "link") else None
             updated = entry.updated if hasattr(entry, "updated") else None
 
-            rss_dic.append({
+            rss_list.append({
                 "title": title,
                 "link": link,
                 "updated": updated
             })
 
-    return rss_dic
+    return rss_list
