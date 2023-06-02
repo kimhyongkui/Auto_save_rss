d99e68b6f606f9ccba5ed8654cc2e17c95bceb53
Kim Hyeong Kyu <hyongkui@nate.com>
2023-06-02 23:56:00 +0900

    파싱한 데이터를 원하는 날짜로 필터링하는 기능 추가

--- api/parse_rss.py
+++ api/parse_rss.py
@@ -1,6 +1,7 @@
 import feedparser
 from datetime import datetime
 
+
 def parsing_rss(keyword):
     donga = "https://rss.donga.com/science.xml"
     hankyung = "https://rss.hankyung.com/feed/it.xml"
@@ -17,19 +18,35 @@ def parsing_rss(keyword):
     ]
     rss_list = []
 
-    today = datetime.today()
+    today = datetime.today().date()
 
     for url in url_list:
         parse_rss = feedparser.parse(url)
-        # 지금 코드를 그대로 사용하면 RSS 피드 메인페이지의 목록을 그대로 긁어옴.
-        # 내가 원하는 날짜의 기사만 필터링 할 필요가 있다.
         for entry in parse_rss.entries:
             title = entry.title if hasattr(entry, "title") else None
             link = entry.link if hasattr(entry, "link") else None
             updated = entry.updated if hasattr(entry, "updated") else None
 
-            if keyword in title and \
-                    datetime.strptime(updated, "%Y-%m-%dT%H:%M:%S%z").date() == today:
+            parsed_updated = None
+
+            # 날짜 형식에 맞게 파싱하여 연도, 월, 일 추출
+            if updated:
+                try:
+                    # "%a, %d %b %Y %H:%M:%S %z" 형식
+                    parsed_updated = datetime.strptime(updated, "%a, %d %b %Y %H:%M:%S %z").date()
+                except ValueError:
+                    try:
+                        # "%Y-%m-%dT%H:%M:%S%z" 형식
+                        parsed_updated = datetime.strptime(updated, "%Y-%m-%dT%H:%M:%S%z").date()
+                    except ValueError:
+                        try:
+                            # "%Y-%m-%d %H:%M:%S" 형식
+                            parsed_updated = datetime.strptime(updated, "%Y-%m-%d %H:%M:%S").date()
+                        except ValueError:
+                            # 다른 형식의 경우에는 날짜 정보를 추출할 수 없음
+                            pass
+
+            if parsed_updated == today and keyword in title:
                 rss_list.append({
                     "title": title,
                     "link": link,
