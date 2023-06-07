227076922c2fce6a1359953360b26718ee5bd5f1
Kim Hyeong Kyu <hyongkui@nate.com>
2023-06-07 23:12:48 +0900

    connectin과 models 작성 + db에 신문사이름+url 저장 기능

---
+++ db/post/rss_url.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import sessionmaker
+from db.connection import engine
+from db.models import Rss_list
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def add_rss(rss_name, rss_url):
+    result = Rss_list(rssNm=rss_name, rssUrl=rss_url)
+    session.add(result)
+    session.commit()
+
+    return "저장 완료"
+
+
