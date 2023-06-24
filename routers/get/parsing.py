f69bc59a1408d384796d7aa8ed6d26f34d895cce
Kim Hyeong Kyu <hyongkui@nate.com>
2023-06-24 23:37:06 +0900

    태그 변경

--- routers/get/parsing.py
+++ routers/get/parsing.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter, Query
 from typing import List
 from api.parse_rss import parsing_rss
 
-router = APIRouter(tags=["파싱"])
+router = APIRouter()
 
 
-@router.get("/feed")
+@router.get("/feed", tags=["파싱"])
 def parsing(keywords: List[str] = Query(...)):
     result = parsing_rss(keywords)
     return result
