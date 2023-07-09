b35eb5f91a3da42fdc672fafb68a5ac519da7694
Kim Hyeong Kyu <hyongkui@nate.com>
2023-07-09 20:28:34 +0900

    RSS Feed의 리스트를 삭제할 수 있는 기능 추가

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from routers.get import parsing, access_token
 from routers.post import rss_url, posting
+from routers.delete import feed
 import uvicorn
 
-app = FastAPI(title="auto-save-rss")
+app = FastAPI(title="Tistory Rss 피드 자동 포스팅")
 
 app.include_router(parsing.router, prefix="/parsing")
 app.include_router(posting.router, prefix="/posting")
 app.include_router(access_token.router, prefix="/access")
 app.include_router(rss_url.router, prefix="/posting")
+app.include_router(feed.router, prefix="/delete")
 
 if __name__ == "__main__":
     uvicorn.run(
