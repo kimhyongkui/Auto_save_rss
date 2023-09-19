09dc974b6300b16413874ac49b567cb10c3a6f66
Kim Hyeong Kyu <hyongkui@nate.com>
2023-09-19 23:55:25 +0900

    ModuleNotFoundError : schedule

--- api/work.py
+++ api/work.py
@@ -1,10 +1,11 @@
 import time
 import schedule
-from api.posting import post_to_tistory
+# from api.posting import post_to_tistory
 
 
 def main():
-    schedule.every().day.at("14:54").do(lambda: post_to_tistory(['AI', '구글', 'GPT', '카카오', '로봇']))
+    # schedule.every().day.at("14:54").do(lambda: post_to_tistory(['AI', '구글', 'GPT', '카카오', '로봇']))
+    schedule.every(3).seconds.do(lambda: print("hello"))
 
     while True:
         schedule.run_pending()
