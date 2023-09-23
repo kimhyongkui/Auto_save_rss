4aa66972209c68f2949ead462e6f570a57db2c29
Kim Hyeong Kyu <hyongkui@nate.com>
2023-09-23 22:39:25 +0900

    함수명 변경

--- api/work.py
+++ api/work.py
@@ -3,13 +3,9 @@ import schedule
 from api.posting import post_to_tistory
 
 
-def main():
+def posting():
     schedule.every().day.at("13:30").do(lambda: post_to_tistory(['AI', '구글', 'GPT', '카카오', '로봇']))
 
     while True:
         schedule.run_pending()
         time.sleep(1)
-
-
-if __name__ == "__main__":
-    main()
